1fb301733b1e87a32a833438faef719db7be66da
Unknown <sgsange@126.com>
2018-09-18 15:49:01 +0800

    备份脚本修改

--- docker-compose/gitlab/gitlab_bakup.sh
+++ docker-compose/gitlab/gitlab_bakup.sh
@@ -2,6 +2,6 @@
 source /etc/profile
 
 backdir=/data/gitlab/data/backups
-/usr/bin/docker exec -i gitlab_gitlab_1  /opt/gitlab/bin/gitlab-rake gitlab:backup:create
+/usr/bin/docker exec -i gitlab  /opt/gitlab/bin/gitlab-rake gitlab:backup:create
 
 find $backdir -mtime +3 -type f | xargs rm -f
