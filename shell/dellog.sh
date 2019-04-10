347fd98d041c37728b4e0c69471c3e506c7631c1
SQGE <30278569+SQGE@users.noreply.github.com>
2019-04-10 09:36:03 +0800

    Create dellog.sh

---
+++ shell/dellog.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+#清除restapi生成的不规则日志
+
+dir=/data/ftp/log
+ls -l $dir | while read i;
+do
+
+
+size=`echo $i | awk '{print $5}'`
+file=`echo $i | awk '{print $9}'`
+
+#大于500m,单位字节
+if [ $size -gt 524288000 ] ;
+then
+
+echo >$file
+
+fi
+done
